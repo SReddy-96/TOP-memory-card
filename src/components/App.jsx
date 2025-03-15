ce0c9e1ce60531b9bc02c54c29e07be50960f4ae
Steven Reddy <steveng.reddy@gmail.com>
2025-03-15 13:49:29 +1300

    Subject: Adding loading and error handling
    Body:
    Adding loading component and error handling to the
    fetch of the API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
 import { useEffect, useState } from "react";
 import "../styles/App.css";
+import Loading from "./loading.jsx";
 
 function App() {
   const [images, setImages] = useState([]);
-
-  console.log("https://dog.ceo/api/breeds/image/random/6");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://dog.ceo/api/breeds/image/random/6")
-      .then((response) => response.json())
-      .then((json) => setImages(json.message));
+    const fetchData = async () => {
+      setLoading(true); // set loading state
+      setError(null); // Reset error state
+
+      try {
+        const response = await fetch(
+          "https://dog.ceo/api/breeds/image/random/6"
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const json = await response.json();
+        setImages(json.message);
+      } catch (e) {
+        setError(e);
+        console.error("Error fetching dog images:", e);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <>
