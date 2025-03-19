2056cedf46aa664a898e7ef940ab00939b9c7a13
Steven Reddy <steveng.reddy@gmail.com>
2025-03-19 20:51:53 +1300

    Subject: Adding a how-to
    Body:
    Adding a little box to show how to play the game
    when the little question mark is hovered over

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,6 +4,17 @@ function Header() {
   return (
     <div id="header">
       <h1>Dog Memory Card</h1>
+
+      <div className="help-container">
+        <span className="help-icon">?</span>
+        <div className="help-tooltip">
+          <h3>How to Play</h3>
+          <p>
+            Click on each image only once. If you click on an image twice, you
+            lose!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
